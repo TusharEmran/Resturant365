1fcd2ff49d6c88a2330ea6a721faec93e22a0a0d
TusharEmran <124170011+TusharEmran@users.noreply.github.com>
2025-03-17 12:41:39 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/Resturant365' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/Manu' element={<Manu/>}/>
